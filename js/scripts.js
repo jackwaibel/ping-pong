0c6a912c071945adb5eabd536034280f3ec67592
Jack Waibel <jackwaibel@gmail.com>
2015-10-16 21:18:26 -0700

    add non negative number alert

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 
 var userNumber = prompt("Please enter any positive integer to get you ping pong on");
 
+if (userNumber >= 0) {
+
   for (var i = 1; i <= userNumber; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
       $('#list').append('<li>'+"ping-pong"+'</li>');}
@@ -11,6 +13,8 @@ var userNumber = prompt("Please enter any positive integer to get you ping pong 
       $('#list').append('<li>'+"ping"+'</li>');}
     else{
       $('#list').append('<li>'+i+'</li>');};
-  };
+    };
+
+  } else alert("INVALID");
 
 });
